5a15e638dd614579fee114c87986301dc8a1b9fe
Tiagoroccatagliata <tiagoroccatagliata@gmail.com>
2024-08-13 10:57:56 -0300

    Actulizacion del CV

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
 
-
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav id="navbar" className="nav">
-      <ul className="nav-list">
-        <li className="nav-left"><a href="/Users/tiagoroccatagliata/mi-portafolio/public/cv.pdf" download="cv.pdf">Download CV</a></li>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
+        <li className="nav-left">
+          <a href="/cvTiagoRoccatagliata.pdf" download="cvTiagoRoccatagliata.pdf">Download CV</a>
+        </li>
         <li><a href="#welcome-section">About</a></li>
         <li><a href="#projects">Work</a></li>
         <li><a href="#contact">Contact</a></li>
